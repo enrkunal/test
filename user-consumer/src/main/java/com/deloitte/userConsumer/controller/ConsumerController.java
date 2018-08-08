34f61cb4033b603a1f0c656013810acb096053d6
kfnu <kfnu@USBLRKFNU1.us.deloitte.com>
2018-08-08 15:55:08 +0530

    aa

--- user-consumer/src/main/java/com/deloitte/userConsumer/controller/ConsumerController.java
+++ user-consumer/src/main/java/com/deloitte/userConsumer/controller/ConsumerController.java
@@ -1,15 +1,11 @@
 package com.deloitte.userConsumer.controller;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestClientException;
@@ -31,7 +27,7 @@ public class ConsumerController {
 		ServiceInstance serviceInstance=instances.get(0);
 		String baseUrl=serviceInstance.getUri().toString();
 		baseUrl=baseUrl+"/users";
-		List<User> users = this.restTemplate.getForObject(baseUrl, List.class);
+		List<User> users = (new RestTemplate()).getForObject(baseUrl, List.class);
 	    return users;
 	}
 	
